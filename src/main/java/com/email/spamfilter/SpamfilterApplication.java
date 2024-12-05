f668b778444bb8736d031cf0acaa96c56e70c75f
jungjiyu <libraryofjiyu@gmail.com>
2024-12-05 16:08:02 +0900

    init

---
+++ src/main/java/com/email/spamfilter/SpamfilterApplication.java
@@ -0,0 +1,13 @@
+package com.email.spamfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamfilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/spamfilter/SpamfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.spamfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
