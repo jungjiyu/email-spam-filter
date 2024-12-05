a567521d216638c34dfc640894950f098b8a1a55
jungjiyu <libraryofjiyu@gmail.com>
2024-12-05 16:55:40 +0900

    Feat : oauth 예시 simple ver 추가

---
+++ src/main/java/com/email/spamfilter/example/simplever/model/UserResource.java
@@ -0,0 +1,12 @@
+package com.email.spamfilter.example.simplever.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResource {
+    String id;
+    String email;
+    String nickname;
+}
